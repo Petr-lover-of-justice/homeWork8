756c735731554fa6dfcd8f9022de2b224fe77dce
Petr <kim.pyoter2017@yandex.ru>
2022-01-20 10:34:55 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+// import "./styles.css";
 
 function App() {
+  const { handleSubmit, register, formState, reset } = useForm();
+
+  const onSubmit = (values) => console.log("ФОРМА!", values);
+
+
+
+  console.log(formState.errors);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <TextField
+          name="firstName"
+          label="Имя"
+          {...register("firstName", {
+            validate: (value) => value !== "admin" || "Nice try!"
+          })}
+          helperText={formState.errors.firstName && formState.errors.firstName.message}
+          error={!!formState.errors.firstName}
+          fullWidth
+        />
+        <TextField
+          name="lastName"
+          label="Фамилия"
+          {...register("lastName", {
+            required: "Это обязательное поле!"
+          })}
+          helperText={formState.errors.lastName && formState.errors.lastName.message}
+          error={!!formState.errors.lastName}
+          fullWidth
+        />
+      </div>
+      <div className="row">
+        <TextField
+          {...register("email", {
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9._%+-]+\.[A-Z]{2,}$/i,
+              message: "Это неверная почта!"
+            }
+          })}
+          helperText={formState.errors.email && formState.errors.email.message}
+          error={!!formState.errors.email}
+          name="email"
+          label="E-Mail"
+          defaultValue=""
+          fullWidth
+        />
+        <TextField
+          {...register("password", {
+            required: "Это обязательное поле!"
+          })}
+          helperText={formState.errors.password && formState.errors.password.message}
+          error={!!formState.errors.password}
+          name="password"
+          type="password"
+          label="Пароль"
+
+          fullWidth
+        />
+      </div>
+      <div className="row">
+        <TextField
+          {...register("email", {
+            pattern: {
+
+            }
+          })}
+          name="about"
+          label="Обо мне"
+          error={!!formState.errors.about}
+          defaultValue=""
+          fullWidth />
+      </div>
+      <br />
+      <div className="row">
+        <Button onClick={handleSubmit(onSubmit)} variant="contained" color="primary">
+          Зарегистрироваться
+        </Button>
+        <Button variant="contained" color="secondary" onClick={() => {
+          reset({
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            about: ""
+          });
+        }}>
+          Очистить
+        </Button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
